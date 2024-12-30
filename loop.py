6562e2ca073a8e1c5f42a2b4405f16f36c5e55ef
Nero Dean <nerodean@Neros-MBP.ssid1>
2024-12-29 20:39:46 -0500

    Loop continues

--- loop.py
+++ loop.py
@@ -4,3 +4,20 @@ i = 0
 while i < len (colors):
 	print(colors[i])
 	i += 1
+
+
+# Range of Length 
+colors = ["red", "green", "blue", "purple"]
+for i in range(len(colors)):
+	print(colors[i])
+
+
+# For-in : the usual way 
+colors = ["red", "green", "blue","purple"]
+for color in colors:
+	print(color)
+
+
+cars = ["Mazda", "Tesla", "Benz", "Toyota", "Honda"]
+for car in cars:
+	print(car)
