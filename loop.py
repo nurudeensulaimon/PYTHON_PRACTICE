8f399ba9f626c742fc34d50f1eb16238fe05b8b9
Nero Dean <nerodean@Neros-MBP.ssid1>
2024-12-29 21:29:13 -0500

    Added enumerate and zip

--- loop.py
+++ loop.py
@@ -21,3 +21,21 @@ for color in colors:
 cars = ["Mazda", "Tesla", "Benz", "Toyota", "Honda"]
 for car in cars:
 	print(car)
+
+presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"]
+for i in range(len(presidents)):
+	print("President {}: {}".format(i+ 1, presidents[i]))
+
+
+# enumerate: does the same thing as above code 
+
+presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"]
+for num, name in enumerate(presidents, start=1):
+	print("President {}: {}".format(num, name))
+
+	# Using Enumerate to loop over two lists 
+sectors = ["Retail", "Computing","Logistics", "Finance", "Entertainment"]
+potentials = [0.2,0.3, 0.1, 0.4,0.5]
+for i, sector in enumerate(sectors):
+	potential = potentials[i]
+	print("{}% {}".format(potential * 100, sector))
