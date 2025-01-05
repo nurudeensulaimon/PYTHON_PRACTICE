5e8902b6c00c325cff4b15307d11e252b3de51f4
Nero Dean <nerodean@Neros-MacBook-Pro.local>
2025-01-05 13:27:15 -0500

    Added Looping Over Multiple Iterables

--- add_matrices.py
+++ add_matrices.py
@@ -1,7 +1,25 @@
 #LOOPING WITH INDEXES 
-
+n = 1
 favorite_fruits = ["jujube", "pear", "watermelon", "apple"]
 for fruit in favorite_fruits:
 	print(n,fruit)
 	n += 1 
-	
+
+
+favorite_fruits = ["jujube", "pear", "watermelon", "apple"]
+for i in range(len(favorite_fruits)):
+	print(i+1, favorite_fruits[i])
+
+
+# Enumerate reurns both index and value 
+for fruit in enumerate(favorite_fruits):
+	print(fruit)
+
+
+# LOOPING OVER MULTIPLE ITERABLES 
+
+fruits = ["loquat", "jujube", "pear", "watermelon", "apple"]
+colors = ["brown", "orange", "green", "pink", "purple"]
+
+for fruit in fruits:
+	print(fruit)
