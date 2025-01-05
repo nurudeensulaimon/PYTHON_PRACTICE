f716f79d31bbbdbf0f54a6af4eae333981384268
Nero Dean <nerodean@Neros-MacBook-Pro.local>
2025-01-05 15:09:43 -0500

    Added Character Map

--- add_matrices.py
+++ add_matrices.py
@@ -23,3 +23,33 @@ colors = ["brown", "orange", "green", "pink", "purple"]
 
 for fruit in fruits:
 	print(fruit)
+
+
+# Using Nested For Loops 
+
+fruits = ["loquat", "jujube", "pear", "watermelon", "apple"]
+colors = ["browns","oranges", "green", "pink", "purple"]
+for fruit in fruits:
+	for color in colors:
+		print(color,fruit)
+
+
+drinks = ["mocha", "java", "capuccinno", "tea", "soda"]
+
+for n, drink in enumerate(drinks, start=1):
+	print(n, drink)
+
+
+# Store index positions of duplicate items 
+characters = ["Krillin", "Goku", "Goku", "Gohan", "Piccolo",
+              "Krillin", "Goku", "Vegeta", "Gohan", "Piccolo",
+              "Piccolo", "Goku", "Vegeta", "Goku", "Piccolo"]
+
+character_map = {character:[] for character in set(characters)}
+print(character_map)
+
+
+# Use enumarate to store the index for each occurrence
+for index, character in enumerate(characters):
+	character_map[character].append(index)
+print(character_map)
