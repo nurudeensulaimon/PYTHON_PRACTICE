1fa22b049f0017bfa535f8f349a974df027297ae
Nero Dean <nerodean@Neros-MacBook-Pro.local>
2025-01-06 09:46:05 -0500

    Add Matrices Function

--- add_matrices.py
+++ add_matrices.py
@@ -47,9 +47,36 @@ characters = ["Krillin", "Goku", "Goku", "Gohan", "Piccolo",
 
 character_map = {character:[] for character in set(characters)}
 print(character_map)
-
+# The format of a list dictionary is that the list cannot be a 
+# key, it can only be valkues 
 
 # Use enumarate to store the index for each occurrence
 for index, character in enumerate(characters):
 	character_map[character].append(index)
 print(character_map)
+
+# NESTED LIST 
+numbers = [1,0,15, [20,-10,15], 17, 0]
+print(numbers[3][0])
+
+
+# PROBLEM STATEMENT 
+matrix1 = [[1,-2], [-3, 4]]
+matrix2 = [[2, -1], [0,-1]]
+
+
+
+def add(matrix1, matrix2):
+	"""Add corresponding numbers in given 2-D matrices."""
+	combined = []
+	for i in range(len(matrix1)):
+		row = []
+		for j in range(len(matrix1[i])):
+			row.append(matrix1[i][j] + matrix2[i][j])
+		combined.append(row)
+	return combined 
+
+test = add(matrix1, matrix2)
+print(test)
+
+
