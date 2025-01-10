a73da44584c9312ef22ffcdf48ee27b763fcb776
Nero Dean <nerodean@Neros-MacBook-Pro.local>
2025-01-10 12:25:25 -0500

    Added Try/Except

--- deep_flatten.py
+++ deep_flatten.py
@@ -6,6 +6,33 @@ expected_output = ["Hong", "Ryan", "Anthony", "Wilhelmina", "Margaret", "Andrian
 
 names = []
 for group in groups:
-	for n in group:
-	   names.append(n)
+	for name in group:
+	   names.append(name)
 print(names)
+
+# FLattening With Comprehension 
+
+names = [
+name 
+for group in groups 
+for name in groups
+]
+
+from itertools import chain 
+chain(*groups)
+
+list(chain(*groups))
+
+numbers = [1,2,3,4,5]
+print(isinstance(numbers,list))
+
+
+try:
+    x=int(input("Enter your favorite integer: "))
+
+except ValueError:
+	print("Uh oh,. That's not a number. Try again")
+else:
+	print(f"I like the number {x}")
+
+
