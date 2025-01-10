fe16a922e58f2ab8bd8e1f5e40d3a0495adccffb
Nero Dean <nerodean@Neros-MBP.ssid1>
2025-01-10 18:24:25 -0500

    Added Bonus1&2

--- deep_flatten.py
+++ deep_flatten.py
@@ -36,3 +36,179 @@ else:
 	print(f"I like the number {x}")
 
 
+def deep_flatten(iterable):
+	"""Flatten an itearable of iterables."""
+	flattened = []
+	for item in iterable:
+		if type(item) in [list, tuple]:
+			for x in deep_faltten(item):
+				flattened.append(x)
+		else:
+			flattened.append(item)
+	return flattened
+
+
+
+def deep_flattened(iterable):
+	"""Flattened an iterable of iterables."""
+	flattened = []
+	for item in iterable:
+		if isinstance(item, (list, tuple)):
+			flattened.extend(deep_flatten(item))
+		else:
+			flattened.append(item)
+	return flattened 
+
+
+
+def deep_flattened(iterable):
+	"""Flatten an iterable of iterables."""
+	flattened = []
+	items = list(iterable)
+	while items:
+		item=items.pop()
+		if isinstance(item, (list, tuple)):
+			items.extend(item)
+		else:
+			flattened.insert(0,item)
+	return flattened
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+	flattened = []
+	items = list(iterable)
+	while items:
+		item=items.pop()
+		if isinstance(item, (list, tuple)):
+			items.extend(item)
+	return reversed(flattened)
+
+
+from collections import deque 
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+	flattened = deque()
+	items = list(iterable)
+	while items:
+		item=items.pop()
+		if isinstance(item, (list, tuple)):
+			items.extend(item)
+		else:
+			flattened.appendleft(item)
+	return flattened 
+
+
+# BONUS #1
+
+def deep_flatten(iterable):
+   """Flatten an iterable of iterables."""
+   flattened = []
+   for item in iterable:
+       try:
+   	       flattened.extend(deep_flatten(item))
+       except TypeError:
+   		   flattened.append(item)
+   return flattened 
+
+from collections.abc import Iterable
+def deep_flatten(iterable):
+    """Flatten an iterable of iterables """ 
+    flattened = []
+    for item in iterable:
+        if isinstance(item, iterable):
+            flattened.extend(deep_flatten(item))  
+        else:
+      	    flattened.append(item)
+    return flattened
+
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+	flattened = []
+	for item in iterable:
+		if hasattr(item, '__Iter__'):
+			flattened.extend(deep_faltten(item))
+		else:
+			flattened.append(item)
+	return flattened 
+
+
+# BONUS #2 
+
+from collections.abc import Iterable 
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+	for item in iterable:
+		if isinstance(item, Iterable):
+			for x in deep_flatten(item):
+				yield x 
+		else:
+			yield item 
+
+
+from collections.abc import Iterable 
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables. """
+	for item in iterable:
+		if isinstance(item, Iterable):
+			yield from deep_flatten(item)
+		else:
+			yield item 
+
+# BONUS #3
+
+x = "hello"
+x[0]
+x[0][0]
+x[0][0][0]
+
+
+from collections.abc import Iterable 
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+	for item in iterable:
+		if isinstance(item,str):
+			yield item 
+		elif isinstance(item, Iterable):
+			yield from deep_flatten(item)
+		else:
+			yield item 
+
+
+
+
+
+
+
+
+# BONUS #2
+
+from collections.abc import Iterable 
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+
+	for item in iterable:
+		if isinstance(item, Iterable):
+		    for x in deep_flatten(item):
+		    	yield x 
+		else:
+			yield item
+
+
+
+from collections.abc import Iterable 
+
+def deep_flatten(iterable):
+	"""Flatten an iterable of iterables."""
+	for item in iterable:
+		if isinstance(item, Iterable):
+			yield from deep_flatten(item)
+		else:
+			yield item
+
+
