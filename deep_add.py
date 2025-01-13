e410fdfbb7764e575018d37f8beabf401313c156
Nero Dean <nerodean@Neros-MBP.ssid1>
2025-01-12 20:49:06 -0500

    Added Zip function for adding two matrices

--- deep_add.py
+++ deep_add.py
@@ -39,3 +39,27 @@ matrix2 = [[5,-4], [3,3]]
 test=add(matrix1, matrix2)
 print(test)
 
+
+# Python ZIP function for looping over two lists 
+
+def add(matrix1, matrix2):
+	"""Add corresponding numbers in given 2-D matrices."""
+	combined = []
+	for rows in zip(matrix1, matrix2):
+		row = []
+		for items in zip(rows[0], rows[1]):
+			row.append(items[0] + items[1])
+		combined.append(row)
+	return combined 
+
+
+def add(matrix1, matrix2):
+	"""Add corresponding numbers in given 2-D matrices."""
+	combined = []
+	for row1, row2 in zip(matrix1, matrix2):
+		row = []
+		for n, m in zip(row1, row2):
+			row.append(n + m)
+		combined.append(row)
+	return combined
+	
